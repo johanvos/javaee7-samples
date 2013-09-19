d2aff275153d59eee74c626cb32d35d8ab7d060f
Joeri Sykora <joeri@sertik.net>
2013-09-19 11:43:18 +0200

    add user on session after registering/login

--- social/user/src/main/java/org/javaee7/social/user/UserHandler.java
+++ social/user/src/main/java/org/javaee7/social/user/UserHandler.java
@@ -3,11 +3,15 @@ package org.javaee7.social.user;
 import com.lodgon.dali.core.entity.User;
 import com.lodgon.dali.core.service.DaliCoreException;
 import com.lodgon.dali.core.service.UserService;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.annotation.ManagedBean;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 /**
@@ -22,11 +26,14 @@ public class UserHandler {
     //UserStorageService userStorage;
     @Inject UserService userService;
 
+		@Context
+		HttpServletRequest request;
+
     @POST
     @Path("create")
     public Response createUser (@FormParam("name") String name
             , @FormParam("email") String email, @FormParam("password") String password)
-            throws DaliCoreException {
+            throws DaliCoreException, URISyntaxException {
         System.out.println("Creating user named "+name);
         System.out.println("userservice = "+userService);
     //    System.out.println("userstorage = "+userStorage);
@@ -36,17 +43,24 @@ public class UserHandler {
         User created = userService.create(user);
         userService.setPassword(created, password);
         System.out.println("Created: "+created);
-        return Response.ok().build();
+
+				request.getSession().setAttribute("user", created);
+        return Response.seeOther(new URI("/user/welcome.jsp")).build();
     }
     
     @POST
     @Path("validate")
     public Response validateUser(@FormParam("email") String email,
-    @FormParam("password") String password) throws DaliCoreException {
+    @FormParam("password") String password) throws DaliCoreException, URISyntaxException {
         System.out.println("email = "+email+" and pwd = "+password);
         User me = userService.validateEmailAndPassword("", email, password);
         System.out.println("Validated? "+me);
-                return Response.ok().build();
 
+				if (me == null) {
+					return Response.seeOther(new URI("/user/index.jsp?login=false")).build();
+				} else {
+					request.getSession().setAttribute("user", me);
+					return Response.seeOther(new URI("/user/welcome.jsp")).build();
+				}
     }
 }
---
+++ social/user/src/main/webapp/welcome.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>JavaEE7 Samples - Social - User</title>
+	</head>
+	<body>
+		<h1>Welcome ${user.screenName}!</h1>
+
+		<p>Your email address is ${user.email}.</p>
+	</body>
+</html>
